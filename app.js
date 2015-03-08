0ce60475dadb1245fdbf4a34540edccfb8e1cc78
Jefferson Lam <jeffersonwlam@gmail.com>
2015-03-08 18:10:33 -0500

    Refactor UI router code
    -Put header into header.html
    -Add footer in footer.html
    -Move home markup into home.html
    -Render the views in index.html

--- app.js
+++ app.js
@@ -29,13 +29,12 @@ app.config([
 '$stateProvider',
 '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('home');
 
   $stateProvider
     .state('home', {
       url: '/home',
-      templateUrl: '/home.html',
+      templateUrl: './home.html',
       controller: 'MainCtrl'
-    });
-
-  $urlRouterProvider.otherwise('home');
+    })
 }]);
