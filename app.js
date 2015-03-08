a9c47ef8e527fa35223fb315030443c218684af0
Jefferson Lam <jeffersonwlam@gmail.com>
2015-03-07 21:26:07 -0600

    Refactor posts into a posts factory

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', ['$scope', function ($scope){
+app.controller('MainCtrl', ['$scope', 'posts', function ($scope, posts){
 	$scope.test = 'Hello World!';
-	$scope.posts = [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}
-	];
+	$scope.posts = posts.posts;
 	$scope.addPost = function(){
 		if(!$scope.title || $scope.title === '') { return; }
 		$scope.posts.push({
@@ -23,3 +17,10 @@ app.controller('MainCtrl', ['$scope', function ($scope){
 		post.upvotes++;
 	};
 }]);
+
+app.factory('posts', function(){
+	var o = {
+		posts: []
+	};
+	return o;
+});
